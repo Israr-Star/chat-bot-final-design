a1491f30a84136b3303d81d4acf837f8edeb99cc
Israr Munim Chowdhury <israr.munim@bjitgroup.com>
2024-12-26 16:21:08 +0600

    getQuery

--- src/twentyFive.js
+++ src/twentyFive.js
@@ -1,30 +1,30 @@
 (function () {
-  // Check if the script is already loaded
   if (window.ChatbotScriptLoaded) return;
   window.ChatbotScriptLoaded = true;
 
-  // Configuration object
   const chatbotConfig = {
-    botUrl: "https://chat-bot-final-design.vercel.app/", // Replace with your chatbot's URL
+    botUrl: "https://chat-bot-final-design.vercel.app/",
     width: "450px",
     height: "660px",
-    position: "bottom-right", // Options: 'bottom-right', 'bottom-left'
+    position: "bottom-right",
+    queryParams: {
+      bgColor: 'red' // Dynamic value
+    }
   };
 
-  // Create iframe
+  const queryString = new URLSearchParams(chatbotConfig.queryParams).toString();
+  const chatbotUrlWithParams = `${chatbotConfig.botUrl}?${queryString}`;
+
   const iframe = document.createElement("iframe");
-  iframe.src = chatbotConfig.botUrl;
+  iframe.src = chatbotUrlWithParams;
   iframe.style.width = chatbotConfig.width;
   iframe.style.height = chatbotConfig.height;
   iframe.style.position = "fixed";
   iframe.style.border = "none";
   iframe.style.zIndex = "9999";
-  // iframe.style.backgroundColor = "#FFFFFF";
   iframe.style.borderRadius = "18px";
-  // iframe.style.display = "none"; // Initially hidden
   iframe.style.allowTransparency = "true";
 
-  // Set position
   if (chatbotConfig.position === "bottom-right") {
     iframe.style.bottom = "0";
     iframe.style.right = "20px";
@@ -33,6 +33,5 @@
     iframe.style.left = "20px";
   }
 
-  // Append iframe to body
   document.body.appendChild(iframe);
 })();
