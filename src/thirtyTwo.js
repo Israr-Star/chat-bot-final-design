b444b2e5737577b64e38535c744e40d5920492b6
Israr Munim Chowdhury <israr.munim@bjitgroup.com>
2024-12-27 09:59:49 +0600

    getQuery10

--- src/thirtyTwo.js
+++ src/thirtyTwo.js
@@ -15,17 +15,17 @@
     width: "450px",
     height: "660px",
     position: "bottom-right",
-    // queryParams: {
-    //   bgColor: param2, // Dynamic value
-    //   headerTitle: paramObj?.headerTitle || "Chat with us",
-    // },
+    queryParams: {
+      bgColor: paramObj?.bgColor, // Dynamic value
+      headerTitle: paramObj?.headerTitle || "Chat with us",
+    },
   };
 
   const queryString = new URLSearchParams(chatbotConfig.queryParams).toString();
   const chatbotUrlWithParams = `${chatbotConfig.botUrl}?${queryString}`;
 
   const iframe = document.createElement("iframe");
-  iframe.src = chatbotConfig.botUrl;
+  iframe.src = chatbotUrlWithParams;
   iframe.style.width = chatbotConfig.width;
   iframe.style.height = chatbotConfig.height;
   iframe.style.position = "fixed";
